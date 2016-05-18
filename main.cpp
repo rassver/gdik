2f39de3b4310b724af7dedd117fe8b8318007b91
Grabovsky Dmytry <grabovskydmytry@gmail.com>
2016-05-18 18:05:09 +0300

    Signed-off-by: Grabovsky Dmytry <grabovskydmytry@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "beta.h"
+
+
+int main()
+{
+    int m;
+    cout<<"Enter number of objects: ";
+    cin>>m;
+    beta *obj1=new beta[m];
+    cout<<"Enter number of objects: ";
+    cout<<"please";
+    int n;
+    cin>>n;
+    beta *obj2=new beta[n];
+    delete [] obj1;
+    delete [] obj3;
+    return 0;
+}
